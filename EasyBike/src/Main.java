84e75984f483baaf991bb46f2ba1ca8a10567be6
Simon <casanosimon@gmail.com>
2022-04-20 17:59:41 +0200

    Import class

--- EasyBike/src/Main.java
+++ EasyBike/src/Main.java
@@ -1,10 +1,12 @@
 import DataAccess.ConnectBD;
+import UserInterface.LoginWindows;
+import UserInterface.RegisterWindow;
 
 public class Main {
     public static void main(String[] args) {
         //LoginWindows firstWindows = new LoginWindows();
         //RegisterWindow registerWindow = new RegisterWindow();
 
-        ConnectBD connection = new ConnectBD();
+        //ConnectBD connection = new ConnectBD();
     }
 }
